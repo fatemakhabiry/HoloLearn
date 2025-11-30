9de5157b322729a6946fdcbd15202de0bb6adb42
mxrxm <mariemossama2003@gmail.com>
2025-11-30 19:26:46 +0200

    7naka fl msg handler

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -70,15 +70,6 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                   const SizedBox(height: AppStyles.spacingXL),
-                                            if (message.isNotEmpty) ...[
-                            const SizedBox(height: AppStyles.spacingL),
-                            MessageDisplay(
-                              massegeBannerSuccess: "Login Succesful",
-                              massegeBannerFail: "Login Failed",
-                              message: message,
-                              onDismiss: () => setState(() => message = ''),
-                            ),
-                          ],
                   // Login Form Card
                   Container(
                     padding: const EdgeInsets.all(AppStyles.spacingL),
@@ -180,6 +171,15 @@ class _LoginPageState extends State<LoginPage> {
                               }
                             },
                           ),
+                          if (message.isNotEmpty) ...[
+                            const SizedBox(height: AppStyles.spacingL),
+                            MessageDisplay(
+                              massegeBannerSuccess: "Login Succesful",
+                              massegeBannerFail: "Login Failed",
+                              message: message,
+                              onDismiss: () => setState(() => message = ''),
+                            ),
+                          ],
                         ],
                       ),
                     ),
